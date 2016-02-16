25a62dd51be7ed740a71c612b39a23be8058f4ea
Tony Kalou <cr4shmaster@hotmail.com>
2016-02-16 18:04:15 +0200

    initial commit

---
+++ modinfo.lua
@@ -0,0 +1,13 @@
+name = "Perishable"
+description = "This mod is required by other mods that are using 'perishable.lua'."
+author = "cr4shmaster"
+version = "1.0.2"
+forumthread = ""
+api_version = 10
+all_clients_require_mod = true
+client_only_mod = false
+dst_compatible = true
+icon_atlas = "modicon.xml"
+icon = "modicon.tex"
+-- server_filter_tags = {""}
+-- priority = 10.00
