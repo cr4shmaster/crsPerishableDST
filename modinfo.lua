79fd1f8269f2ba41d6c97de3e2173ddb89c67d18
cr4shmaster <cr4shmaster@hotmail.com>
2018-04-20 14:26:07 +0300

    minow tweaks

--- modinfo.lua
+++ modinfo.lua
@@ -1,9 +1,9 @@
 -- More information here: https://sites.google.com/view/cr4shmaster/perishable-ds-dst
 
-name = "Perishable v2.1.2.1"
+name = "Perishable v2.1.2.2"
 description = "This mod is required by other mods that are using 'perishable.lua'."
 author = "cr4shmaster"
-version = "2.1.2.1"
+version = "2.1.2.2"
 forumthread = ""
 api_version = 10
 all_clients_require_mod = true
@@ -13,3 +13,7 @@ icon_atlas = "modicon.xml"
 icon = "modicon.tex"
 server_filter_tags = {"perishable"}
 -- priority = 10.00
+
+configuration_options = {
+    {name = "cfgTestCheck", label = "Installed", options = {{description = "Yes", data = true}}, default = true,},
+}
